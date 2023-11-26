4ed32a373f1b3d683be8b6cc03bbaeffe89ce130
Segoy <segoy.thiel@protonmail.com>
2023-11-26 10:33:49 +0100

    added Layout and some functionality to Recipes Section

--- src/app/header/recipes/recipe-list/recipe-list.component.ts
+++ src/app/header/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe',
+      'This is the Test Recipe',
+      'https://hips.hearstapps.com/hmg-prod/images/crepes-lead-64347419487e4.jpg?crop=0.9995238095238095xw:1xh;center,top&resize=980:*'),
+    new Recipe('second Test Recipe', 'Still a Test', 'https://hips.hearstapps.com/hmg-prod/images/crepes-lead-64347419487e4.jpg?crop=0.9995238095238095xw:1xh;center,top&resize=980:*')
+  ];
+
+  constructor() {
+  }
 
 }
