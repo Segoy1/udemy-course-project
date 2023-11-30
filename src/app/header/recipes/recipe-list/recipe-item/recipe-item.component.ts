d1251735efcf070cf9816b899f5bd279605da2ba
Segoy <segoy.thiel@protonmail.com>
2023-11-30 09:34:19 +0100

    added Recipe Select Logic

--- src/app/header/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/header/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Recipe} from "../../recipe.model";
 
 @Component({
@@ -8,6 +8,7 @@ import {Recipe} from "../../recipe.model";
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
+  @Output() recipeEmitter: EventEmitter<Recipe> = new EventEmitter<Recipe>();
 
   constructor() {
   }
--- src/app/header/recipes/recipe-list/recipe-list.component.ts
+++ src/app/header/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import {Recipe} from "../recipe.model";
 
 @Component({
@@ -7,6 +7,7 @@ import {Recipe} from "../recipe.model";
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  @Output() recipeEvent:EventEmitter<Recipe> = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Test Recipe',
       'This is the Test Recipe',
@@ -15,6 +16,10 @@ export class RecipeListComponent {
   ];
 
   constructor() {
+  }
+
+  onClick(recipe: Recipe){
+    this.recipeEvent.emit(recipe);
   }
 
 }
