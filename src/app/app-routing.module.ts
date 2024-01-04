1aeb002daf98fecdc19c392a17f55d8f8d432e12
Segoy <segoy.thiel@protonmail.com>
2024-01-04 11:17:19 +0100

    added router children to use Router Links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,18 @@ import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {RecipesComponent} from "./header/recipes/recipes.component";
 import {ShoppingListComponent} from "./header/shopping-list/shopping-list.component";
+import {RecipeStartComponent} from "./header/recipes/recipe-start/recipe-start.component";
+import {RecipeDetailComponent} from "./header/recipes/recipe-detail/recipe-detail.component";
 
-const appRoutes:Routes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  { path: 'recipes', component: RecipesComponent },
-  { path: 'shopping-list', component: ShoppingListComponent }
+const appRoutes: Routes = [
+  {path: '', redirectTo: '/recipes', pathMatch: 'full'},
+  {
+    path: 'recipes', component: RecipesComponent, children: [
+      {path: '', component: RecipeStartComponent},
+      {path: ':id', component: RecipeDetailComponent}
+    ]
+  },
+  {path: 'shopping-list', component: ShoppingListComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import {FormsModule} from "@angular/forms";
 import {DropdownDirective} from "./header/shared/dropdown.directive";
 import {AppRoutingModule} from "./app-routing.module";
 import {ShoppingListService} from "./header/shopping-list/shopping-list.service";
+import {RecipeStartComponent} from "./header/recipes/recipe-start/recipe-start.component";
+import { RecipeEditComponent } from './header/recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,9 @@ import {ShoppingListService} from "./header/shopping-list/shopping-list.service"
     RecipeDetailComponent,
     ShoppingListEditComponent,
     RecipesComponent,
-    DropdownDirective
+    DropdownDirective,
+    RecipeStartComponent,
+    RecipeEditComponent
   ],
   imports: [
     FormsModule,
