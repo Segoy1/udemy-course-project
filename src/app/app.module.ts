2ee26138186db01a64eb37f0a8e96f80f71df53f
Segoy <segoy.thiel@protonmail.com>
2024-01-15 09:11:27 +0100

    Optimized Imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './header/shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './header/shopping-list/shopping-list-edit/shopping-list-edit.component';
 import {RouterLink} from "@angular/router";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {DropdownDirective} from "./header/shared/dropdown.directive";
--- src/app/header/recipes/recipes.module.ts
+++ src/app/header/recipes/recipes.module.ts
@@ -7,8 +7,7 @@ import {RecipeEditComponent} from "./recipe-edit/recipe-edit.component";
 import {RecipesComponent} from "./recipes.component";
 import {RouterModule} from "@angular/router";
 import {CommonModule} from "@angular/common";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {AppRoutingModule} from "../../app-routing.module";
+import {ReactiveFormsModule} from "@angular/forms";
 import {RecipesRoutingModule} from "./recipes-routing.module";
 
 @NgModule({
